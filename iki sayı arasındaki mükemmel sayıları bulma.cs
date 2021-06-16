4a5455bb2765154069db441536b2eec9215dcbb2
omfagu <omergurbuz25@hotmail.com>
2021-06-16 15:01:56 +0300

    Create iki sayı arasındaki mükemmel sayıları bulma.cs

---
+++ iki sayı arasındaki mükemmel sayıları bulma.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace sınav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam;
+            int sayi;
+            int islem;
+            for (sayi = 1; sayi < 10000; sayi++)
+            {
+                toplam = 0;
+                for (islem = 1; islem < sayi; islem++)
+                {
+                    if (sayi % islem == 0)
+                    {
+                        toplam = toplam + islem;
+                    }
+                }
+                if (sayi == toplam)
+                {
+                    Console.WriteLine(sayi + ": Mükemmel Sayıdır.");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+}
